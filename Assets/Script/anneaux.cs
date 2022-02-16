c99ca4d8310e6d730821a5e3ae579684e5b2046e
TLBail <62939298+TLBail@users.noreply.github.com>
2022-02-15 21:52:34 -0500

    anneaux + score clean code mon pote 🤜🤛

---
+++ Assets/Script/anneaux.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using Object = UnityEngine.Object;
+
+public class anneaux : MonoBehaviour
+{
+    [SerializeField] private Object explosion;
+    [SerializeField] private String tag = "Player";
+
+    
+    [SerializeField] private ScoreManager scoreManager;
+
+    [SerializeField] private Transform positionDeLexplosion;
+
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.tag);
+        if (other.CompareTag(tag))
+        {
+            Instantiate(explosion, positionDeLexplosion.position, Quaternion.Euler(Vector3.zero));
+            scoreManager.addScore();
+            Invoke("hide", 0.5f); 
+        }
+    }
+
+    private void hide()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Script/playerMovement.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 5.0f;
+    
+        private void Update()
+        {
+            var horizontal = Input.GetAxis("Horizontal");
+            var vertical = Input.GetAxis("Vertical");
+            transform.Translate(new Vector3(horizontal, 0, vertical) * (speed * Time.deltaTime));
+        }
+}
