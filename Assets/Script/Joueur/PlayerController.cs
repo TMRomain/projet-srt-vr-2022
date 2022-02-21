8416482e88a1278dcef0b2bbf0aabb56d0eab00e
TMRomain <romainover51@gmail.com>
2022-02-21 18:15:51 -0500

    Ajout des variable de controle

---
+++ Assets/Script/Joueur/PlayerController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform mainDroite;
+
+
+    [SerializeField]
+    Transform mainGauche;
+
+
+    [SerializeField]
+    Transform cameraPosition;
+
+
+
+    [SerializeField]
+    float vitesseAccendente = 50;
+    float resistanceAir = 10;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
