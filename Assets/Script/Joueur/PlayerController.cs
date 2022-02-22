e8f9f993b341e473359ceb74be8a312b650e1ed2
TMRomain <romainover51@gmail.com>
2022-02-21 21:26:32 -0500

    Fix detection marge + mouvement avant du joueur.

--- Assets/Script/Joueur/PlayerController.cs
+++ Assets/Script/Joueur/PlayerController.cs
@@ -19,6 +19,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     GameObject player;
     float valeurRotationHaut = 90;
+    float valeurRotationAvant = 0;
 
     [SerializeField]
     float margeDetection = 10;
@@ -41,22 +42,29 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(estAuSol());
+        //Debug.Log(estAuSol());
         //Ajouter && estDansMarge(mainGauche.eulerAngles.x,valeurRotationHaut,margeDetection) quand relier au casque
         if(estDansMarge(mainDroite.eulerAngles.x,valeurRotationHaut,margeDetection) ){
             player.GetComponent<Rigidbody>().AddForce(new Vector3(0,vitesseAccendente * Time.deltaTime,0));
         }
+        if(!estAuSol() && estDansMarge(mainDroite.eulerAngles.x,valeurRotationAvant,margeDetection)){
+            player.GetComponent<Rigidbody>().AddForce(new Vector3(0,0,-vitesseAccendente * Time.deltaTime));
+        }
+        // Debug.Log(estDansMarge(mainDroite.eulerAngles.x,valeurRotationHaut,margeDetection));
         // Debug.Log(mainDroite.eulerAngles.x);
     }
     
 
     bool estDansMarge(float valeur,float valeurSouhaiter,float marge){
+        // Debug.Log(valeur);
+        // Debug.Log(valeurSouhaiter+marge );
+        // Debug.Log(valeurSouhaiter-marge);
         if( valeur <= valeurSouhaiter+marge ){
-            if(valeurSouhaiter-marge >= valeur ){
-                return false;
+            if(valeurSouhaiter-marge <= valeur ){
+                return true;
             }
         }
-        return true;
+        return false;
     }
     bool estAuSol(){
         return Physics.Raycast(player.transform.position, -Vector3.up, distSol + 0.1f);
