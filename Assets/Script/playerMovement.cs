792414662a7f9590bf6bb244970cc3f42cdb5778
TLBail <62939298+TLBail@users.noreply.github.com>
2022-02-25 10:39:49 -0500

    HUD + Score + Chrono = 👌

--- Assets/Script/playerMovement.cs
+++ Assets/Script/playerMovement.cs
@@ -11,5 +11,16 @@ public class playerMovement : MonoBehaviour
             var horizontal = Input.GetAxis("Horizontal");
             var vertical = Input.GetAxis("Vertical");
             transform.Translate(new Vector3(horizontal, 0, vertical) * (speed * Time.deltaTime));
+
+            if (Input.GetKey(KeyCode.E))
+            {
+                transform.Translate(Vector3.up * (Time.deltaTime * speed));
+            }
+        
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(Vector3.down * (Time.deltaTime * speed));
+            }
+            
         }
 }
