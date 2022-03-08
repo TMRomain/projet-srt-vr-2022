8d3c4fa1836659768a33facd8383219e5b5fd411
antolefou <simon72610@gmail.com>
2022-03-08 10:48:18 -0500

    Les mouvements du joueur peuvent suivre la caméra

--- Assets/Script/playerMovement.cs
+++ Assets/Script/playerMovement.cs
@@ -5,22 +5,34 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 5.0f;
-    
-        private void Update()
+    [SerializeField] private Transform camera;
+    [SerializeField] bool cameraControl = true;
+
+    private void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        if (!cameraControl)
         {
-            var horizontal = Input.GetAxis("Horizontal");
-            var vertical = Input.GetAxis("Vertical");
+                
             transform.Translate(new Vector3(horizontal, 0, vertical) * (speed * Time.deltaTime));
 
             if (Input.GetKey(KeyCode.E))
             {
                 transform.Translate(Vector3.up * (Time.deltaTime * speed));
             }
-        
+
             if (Input.GetKey(KeyCode.A))
             {
                 transform.Translate(Vector3.down * (Time.deltaTime * speed));
             }
-            
+
         }
+        else
+        {
+            transform.Translate(camera.forward * (Time.deltaTime * speed * vertical));
+        }
+                       
+    }
 }
